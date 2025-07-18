b70ef439ed6dc96f07a64f9bb85e3264b2c67ae8
createdbyharsh <createdbyharsh@gmail.comgit config --global user.email createdbyharsh@gmail.com>
2025-07-18 17:04:58 +0530

    before separate components are made

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,4 +1,43 @@
 import React, { useState } from "react";
 
+function App(){
+  const [input, setInput]= useState("")
+  const [items ,setItems] = useState([])
+
+function handleChange(e){
+  setInput(e.target.value)
+}
+
+function handleClick(){
+  setItems((previousItems) => { 
+    return [...previousItems, input]
+    
+   })
+   setInput("");
+}
+
+  return (
+  <div className="container">
+    <div className="heading">
+      <h1>To-Do list</h1>
+    </div>
+    <div className="form">
+      <input type="text" onChange = {handleChange} value = {input}/>
+      <button onClick = {handleClick}>
+        <span>
+          Add
+        </span>
+      </button>
+    </div>
+    <div className="listArea">
+      <ul>
+        {items.map((x)=>{
+          return <li>{x}</li>
+        })}
+      </ul>
+    </div>
+  </div>
+  )
+}
 
 export default App;
--- src/index.jsx
+++ src/index.jsx
@@ -1,8 +1,17 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import App from "./components/App";
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import { BrowserRouter } from "react-router-dom";
 
-ReactDOM.render(<App />, document.getElementById("root"));
+createRoot(document.getElementById("root")).render(
+  <StrictMode>
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  </StrictMode>
+);
 
 //CHALLENGE: I have extracted the Input Area, including the <input> and
 //<button> elements into a seperate Component called InputArea.
